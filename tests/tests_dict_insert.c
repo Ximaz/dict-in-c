a920be6ebaaa1c24075cb73541bb8db387339748
DURAND Malo <malo.durand@epitech.eu>
2024-12-22 14:16:57 +0100

    feat: adding tests for dict_insert.

---
+++ tests/tests_dict_insert.c
@@ -0,0 +1,21 @@
+/*
+** XIMAZ PROJECTS, 2024
+** tests_dict_ctor.c
+** File description:
+** Unit tests for the dict constructor and destructor.
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <criterion/criterion.h>
+#include <criterion/new/assert.h>
+#include "dict.h"
+
+Test(dict_insert, passing)
+{
+    dict_t *dict = dict_ctor();
+    char *my_value = "Hello, World !";
+
+    cr_expect(eq(int, 0, dict_insert(dict, "KEY", 3, (void *) my_value)));
+    dict_dtor(dict);
+}
